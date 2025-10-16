263b302298f4189537dbf4c4890d40a18dc03255
Ronak <ronakrathod0022@gmail.com>
2025-10-16 21:03:04 +0530

    add LogoutBtn component for user logout functionality

---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../../appwrite/auth'
+import { logout } from '../../features/authSlice'
+
+
+
+function LogoutBtn() {
+    const dispatch = useDispatch();
+
+    const logoutHandler =( ) => {
+        authService.logout()
+        .then(() => {
+            dispatch(logout())
+        })
+        .catch((err) => {
+            console.log("error at logoutHandler :: LogoutBtn",err);
+        })
+    }
+  return (
+   <button
+    className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+   >Logout</button>
+  )
+}
+
+export default LogoutBtn
