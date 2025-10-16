1c95ba6fd2552f82138b093004fbd39f0437bbf1
Ronak <ronakrathod0022@gmail.com>
2025-10-16 20:25:40 +0530

    fix: clean up import statements in store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,9 @@
 import { configureStore }  from "@reduxjs/toolkit";
-
+import authSlice from "../features/authSlice"
 
 const store = configureStore({
     reducer : {
-        
+        auth : authSlice        
     }
 })
 
