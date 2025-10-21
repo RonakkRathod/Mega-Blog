cf0ca3fb305f7fcc10ccd71ae18ac663a01ed557
Ronak <ronakrathod0022@gmail.com>
2025-10-21 20:00:56 +0530

    add index.js to export page components for blog functionality

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Home from "./Home"
+import AllPosts from "./AllPosts"
+import EditPost from "./EditPost"
+import AddPost from "./AddPost"
+import Login from "./Login"
+import Post from "./Post"
+import SignUp from "./SignUp"
+
+export {
+    Home,
+    AllPosts,
+    EditPost,
+    AddPost,
+    Login,
+    Post,
+    SignUp
+}
