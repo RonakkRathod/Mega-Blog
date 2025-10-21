4112a88371409718e54c1763bc111159058b8cb2
Ronak <ronakrathod0022@gmail.com>
2025-10-21 19:37:34 +0530

    add Home and SignUp components for main page and user registration

---
+++ src/components/pages/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Signup as signupComponent} from '../index'
+
+function SignUp() {
+  return (
+    <div className='py-8'>
+        <signupComponent />
+    </div>
+  )
+}
+
+export default SignUp
